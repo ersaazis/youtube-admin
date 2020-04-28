c844f01a954559f6603cbbc6b3b2014463b1773d
ersaazis <eam24maret@gmail.com>
2020-04-28 10:46:01 +0700

    javascript 2

--- app/Http/Controllers/GetUrlController.php
+++ app/Http/Controllers/GetUrlController.php
@@ -9,7 +9,6 @@ class GetUrlController extends Controller
 {
     public function url($id){
         $youtube=DB::table('youtube')->find($id);
-        DB::table('youtube')->where('id',$id)->update(["view"=>$youtube->view+1]);
         return "
         if('$youtube->url' != document.URL){
             window.location.href = '$youtube->url';
@@ -21,6 +20,7 @@ class GetUrlController extends Controller
         $referer=$youtube->referer;
         $referer=explode("\n",$referer);
         $ref=$referer[rand(0,count($referer)-1)];
+        DB::table('youtube')->where('id',$id)->update(["view"=>$youtube->view+1]);
         return redirect($ref);
     }
 }
